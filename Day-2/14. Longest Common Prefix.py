c0638fa0577ab9003e7e0f3db3f6867ad748aefa
tejasbariya <tejasbariya22@gmail.com>
2025-09-27 19:55:08 +0530

    Day 2

---
+++ Day-2/14. Longest Common Prefix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+
+        if len (strs) == 0: return ""
+        if len(strs) == 1:
+            return strs
+        minlen = len(strs[0])
+        for i in range((len(strs))):
+            if len(strs[i]) < minlen :
+                minlen=len(strs[i])
+
+        flag  = True
+        lcp = ''
+        for i in range(minlen):
+            temp = strs[0][i]
+            for word in strs:
+                if temp == word[i]:
+                    continue
+                else :
+                    flag = False
+                    break
+            if flag :
+                lcp += temp
+            else :
+                return lcp
+
+        return lcp
+
+print(Solution().longestCommonPrefix(["ab","a","aflight"]))
