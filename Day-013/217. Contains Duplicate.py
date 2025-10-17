164048293f8cf98d122ada57ddabc8b8c692a4e6
tejasbariya <tejasbariya22@gmail.com>
2025-10-17 13:37:34 +0530

    Day 13 (Updated)

--- Day-013/217. Contains Duplicate.py
+++ Day-013/217. Contains Duplicate.py
@@ -1,7 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        nums.sort()
-        for n1 in range(len(nums) -1):
-                if nums[n1] == nums[n1 +1]:
-                    return True
+        visited = set()
+        for i in range(len(nums)):
+            if nums[i] in visited:
+                  return True
+            visited.add(nums[i])
         return False
