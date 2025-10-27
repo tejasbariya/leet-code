88be175f9ada9937e3cd2e225710be522617f5e2
tejasbariya <tejasbariya22@gmail.com>
2025-10-27 15:32:54 +0530

    Day 18

---
+++ Day-018/303. Range Sum Query - Immutable.py
@@ -0,0 +1,18 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+
+    def sumRange(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        sum = 0
+        for i in range(left, right+1):
+            sum += self.nums[i]
+        return sum 
