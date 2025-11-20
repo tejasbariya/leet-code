7da7d01bbd788fdb4babeb9a815dfe1ab63787d4
tejasbariya <tejasbariya22@gmail.com>
2025-11-20 18:16:16 +0530

    Day 5

--- Day-005/35. Search Insert Position.py
+++ Day-005/35. Search Insert Position.py
@@ -6,10 +6,9 @@ class Solution:
             mid = start + (end - start) //2 
             if nums[mid] == target:
                 return mid
-            print(f"start:{start}, mid:{mid}, end:{end}, num:{nums[mid]}")
             if nums[mid] < target:
-                end = mid - 1
-            else:
                 start = mid + 1
+            else:
+                end = mid - 1
 
         return start
