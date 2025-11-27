6cdebaa2d6f9c4948c3da2e24f37f7ad1129a8d1
Tejas Bariya <tejasbariya22@gmail.com>
2025-11-27 23:57:56 +0530

    Day 46

---
+++ Day-046/704. Binary Search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0 , len(nums) -1
+        while l <= r:
+            mid = l + (r - l)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                l = mid +1
+            else: 
+                r = mid -1
+        return -1 
+        
