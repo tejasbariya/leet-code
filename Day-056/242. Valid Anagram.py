b569b92fb39c0568cc60b64d684a1ff13426a532
tejasbariya <tejasbariya22@gmail.com>
2025-12-13 18:49:10 +0530

    Day 56

---
+++ Day-056/242. Valid Anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        for char in set(s):
+            if s.count(char) != t.count(char):
+                return False
+        return True
